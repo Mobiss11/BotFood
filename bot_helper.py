4fe468ca71dbfe341bca5b77e2cb8d3a45c1c620
Aleksy <aslepaugo@gmail.com>
2022-09-25 15:24:57 +0200

    fix regex

--- bot_helper.py
+++ bot_helper.py
@@ -4,7 +4,7 @@ from models import Meal
 
 
 def is_phone_valid(phone_number):
-    rgx_phone = re.compile("(?:+?(?d{2,3}?)?D?)?d{4}D?d{4}")
+    rgx_phone = re.compile(r"((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}")
     if re.findall(rgx_phone, phone_number):
         return True
     return False
